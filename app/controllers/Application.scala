42995d460d4119e97cb2f0b6d34fde09033226ba
AKB428 <otoraru@gmail.com>
2015-05-02 18:35:25 +0900

    db to JSON test code

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,6 +4,7 @@ import play.api._
 import play.api.db._
 import play.api.mvc._
 import play.api.Play.current
+import play.api.libs.json.Json
 import anorm._
 
 object Application extends Controller {
@@ -27,4 +28,13 @@ object Application extends Controller {
     }
   }
 
+  def testjson = Action {
+    DB.withConnection { implicit c =>
+      val bases = SQL("SELECT * FROM BASES ORDER BY ID")().map {
+        row => (row[Int]("ID").toString, row[String]("title"))
+      }.toMap[String, String]
+
+      Ok(Json.toJson(bases))
+    }
+  }
 }

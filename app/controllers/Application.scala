6394d998f14f9ee2ae7c47f0c1a26d65421e4a0c
AKB428 <otoraru@gmail.com>
2015-05-05 15:06:11 +0900

    routeから不要なパスを削除

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -10,31 +10,6 @@ import anorm._
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
-
-  def test = Action {
-    DB.withConnection { implicit c =>
-      val bases = SQL("SELECT * FROM BASES ORDER BY ID")().map {
-        row => (row[Int]("ID"), row[String]("title"))
-      }
-      Ok(<ul>
-        {bases.map { case (id, title) => <li>
-          {id}
-          -
-          {title}
-        </li>
-        }}</ul>).as(HTML)
-    }
-  }
-
-  def testjson = Action {
-    DB.withConnection { implicit c =>
-      val bases = SQL("SELECT * FROM BASES ORDER BY ID")().map {
-        row => (row[Int]("ID").toString, row[String]("title"))
-      }.toMap[String, String]
-
-      Ok(Json.toJson(bases))
-    }
+    Ok(views.html.index("ShangriLa Anime API Server"))
   }
 }
